38599a1424665189d0873778498d4723493a912e
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-03-07 14:50:35 +0900

    Remove useless payload

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { TCombinedStates } from './modules';
 import { Dispatch } from 'redux';
 
 export interface ICounterActions {
-  readonly increment: (number: number) => void
-  readonly decrement: (number: number) => void
+  readonly increment: () => void
+  readonly decrement: () => void
 }
 
 export interface IAppProps {
@@ -19,19 +19,11 @@ class App extends Component<IAppProps> {
   render() {
     const { CounterActions, number } = this.props
 
-    const handleIncrement = (e: SyntheticEvent) => {
-      CounterActions.increment(number);
-    }
-
-    const handleDecrement = (e: SyntheticEvent) => {
-      CounterActions.decrement(number)
-    }
-
     return (
       <div>
         <h1>{number}</h1>
-        <button onClick={handleIncrement}>+</button>
-        <button onClick={handleDecrement}>-</button>
+        <button onClick={CounterActions.increment}>+</button>
+        <button onClick={CounterActions.decrement}>-</button>
       </div>
     );
   }
@@ -40,8 +32,8 @@ class App extends Component<IAppProps> {
 const mapStateToProps = (state: TCombinedStates) => ({ number: state.counter })
 const mapDispatchToProps = (dispatch: Dispatch<counterActions.TCounterActions>) => ({
   CounterActions: {
-    increment: (number: number) => dispatch(counterActions.increment(number)),
-    decrement: (number: number) => dispatch(counterActions.decrement(number))
+    increment: () => dispatch(counterActions.increment()),
+    decrement: () => dispatch(counterActions.decrement())
   }
 })
 
